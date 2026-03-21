96af24ca5f711e75b20be2fd3e643b5dc8856600
elbos2 <ellaubo@hotmail.com>
2026-03-21 12:42:39 +0100

    Update bashfile

--- scripts/bash/clone_wordpress_vms.sh
+++ scripts/bash/clone_wordpress_vms.sh
@@ -4,12 +4,11 @@
 # Kloon template VM 9000 naar 6 WordPress VM's
 # Uitvoeren op pve-node1 als root
 
-set -e
-
 TEMPLATE_ID=9000
 STORAGE="ceph-pool"
 GATEWAY="10.24.40.1"
-BRIDGE="vmbr0"
+SSH_USER="ellaubo"
+SSH_PORT="5995"
 
 declare -A VM_CONFIG=(
   [101]="pve-node1,10.24.40.21"
@@ -20,9 +19,16 @@ declare -A VM_CONFIG=(
   [106]="pve-node3,10.24.40.26"
 )
 
-for VMID in "${!VM_CONFIG[@]}"; do
+declare -A NODE_IPS=(
+  [pve-node1]="10.24.40.2"
+  [pve-node2]="10.24.40.3"
+  [pve-node3]="10.24.40.4"
+)
+
+for VMID in 101 102 103 104 105 106; do
   NODE=$(echo ${VM_CONFIG[$VMID]} | cut -d',' -f1)
   IP=$(echo ${VM_CONFIG[$VMID]} | cut -d',' -f2)
+  NODE_IP=${NODE_IPS[$NODE]}
 
   echo "=== Kloon VM ${VMID} op ${NODE} met IP ${IP} ==="
 
@@ -32,11 +38,21 @@ for VMID in "${!VM_CONFIG[@]}"; do
     --storage ${STORAGE} \
     --target ${NODE}
 
-  qm set ${VMID} \
-    --ipconfig0 ip=${IP}/24,gw=${GATEWAY} \
-    --ciuser ubuntu \
-    --cipassword Welkom01x \
-    --sshkeys /root/.ssh/id_rsa.pub
+  echo "=== Configureer VM ${VMID} ==="
+  if [ "${NODE}" == "pve-node1" ]; then
+    qm set ${VMID} \
+      --ipconfig0 ip=${IP}/24,gw=${GATEWAY} \
+      --ciuser ubuntu \
+      --cipassword Welkom01x \
+      --sshkeys /root/.ssh/id_rsa.pub
+  else
+    ssh -p ${SSH_PORT} ${SSH_USER}@${NODE_IP} \
+      "sudo qm set ${VMID} \
+        --ipconfig0 ip=${IP}/24,gw=${GATEWAY} \
+        --ciuser ubuntu \
+        --cipassword Welkom01x \
+        --sshkeys /root/.ssh/id_rsa.pub"
+  fi
 
   qm start ${VMID}
 
