515b1bd68d7cd62de7dfe7a2ce7c365d0d38e3a6
elbos2 <ellaubo@hotmail.com>
2026-03-21 13:37:34 +0100

    Update bash wordpress file to add config without password

--- scripts/bash/clone_wordpress_vms.sh
+++ scripts/bash/clone_wordpress_vms.sh
@@ -11,29 +11,23 @@ SSH_USER="ellaubo"
 SSH_PORT="5995"
 
 declare -A VM_CONFIG=(
-  [101]="pve-node1,10.24.40.21"
-  [102]="pve-node1,10.24.40.22"
-  [103]="pve-node2,10.24.40.23"
-  [104]="pve-node2,10.24.40.24"
-  [105]="pve-node3,10.24.40.25"
-  [106]="pve-node3,10.24.40.26"
-)
-
-declare -A NODE_IPS=(
-  [pve-node1]="10.24.40.2"
-  [pve-node2]="10.24.40.3"
-  [pve-node3]="10.24.40.4"
+  [101]="pve-node1,10.24.40.2,10.24.40.21"
+  [102]="pve-node1,10.24.40.2,10.24.40.22"
+  [103]="pve-node2,10.24.40.3,10.24.40.23"
+  [104]="pve-node2,10.24.40.3,10.24.40.24"
+  [105]="pve-node3,10.24.40.4,10.24.40.25"
+  [106]="pve-node3,10.24.40.4,10.24.40.26"
 )
 
 for VMID in 101 102 103 104 105 106; do
   NODE=$(echo ${VM_CONFIG[$VMID]} | cut -d',' -f1)
-  IP=$(echo ${VM_CONFIG[$VMID]} | cut -d',' -f2)
-  NODE_IP=${NODE_IPS[$NODE]}
+  NODE_IP=$(echo ${VM_CONFIG[$VMID]} | cut -d',' -f2)
+  VM_IP=$(echo ${VM_CONFIG[$VMID]} | cut -d',' -f3)
 
-  echo "=== Kloon VM ${VMID} op ${NODE} met IP ${IP} ==="
+  echo "=== Kloon VM ${VMID} op ${NODE} met IP ${VM_IP} ==="
 
   qm clone ${TEMPLATE_ID} ${VMID} \
-    --name "wordpress-$(echo $IP | cut -d'.' -f4)" \
+    --name "wordpress-$(echo $VM_IP | cut -d'.' -f4)" \
     --full \
     --storage ${STORAGE} \
     --target ${NODE}
@@ -41,22 +35,22 @@ for VMID in 101 102 103 104 105 106; do
   echo "=== Configureer VM ${VMID} ==="
   if [ "${NODE}" == "pve-node1" ]; then
     qm set ${VMID} \
-      --ipconfig0 ip=${IP}/24,gw=${GATEWAY} \
+      --ipconfig0 ip=${VM_IP}/24,gw=${GATEWAY} \
       --ciuser ubuntu \
       --cipassword Welkom01x \
       --sshkeys /root/.ssh/id_rsa.pub
+    qm start ${VMID}
   else
     ssh -p ${SSH_PORT} ${SSH_USER}@${NODE_IP} \
       "sudo qm set ${VMID} \
-        --ipconfig0 ip=${IP}/24,gw=${GATEWAY} \
+        --ipconfig0 ip=${VM_IP}/24,gw=${GATEWAY} \
         --ciuser ubuntu \
         --cipassword Welkom01x \
-        --sshkeys /root/.ssh/id_rsa.pub"
+        --sshkeys /root/.ssh/id_rsa.pub && \
+       sudo qm start ${VMID}"
   fi
 
-  qm start ${VMID}
-
-  echo "=== VM ${VMID} gestart op ${NODE} met IP ${IP} ==="
+  echo "=== VM ${VMID} gestart op ${NODE} met IP ${VM_IP} ==="
 done
 
 echo "=== Alle VM's gekloond en gestart ==="
